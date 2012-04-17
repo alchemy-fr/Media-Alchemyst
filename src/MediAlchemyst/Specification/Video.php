4a9a4a285197f214c6602f7d64e1f418150b0877
Romain Neutron <imprec@gmail.com>
2012-04-17 17:57:26 +0200

    Add video specifications

---
+++ src/MediAlchemyst/Specification/Video.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MediAlchemyst\Specification;
+
+use MediAlchemyst\Exception;
+
+class Video extends Audio
+{
+
+    const FILETYPE_X264 = 'x264';
+    const FILETYPE_WEBM = 'WebM';
+    const FILETYPE_OGG  = 'Ogg';
+
+    public function getType()
+    {
+        return self::TYPE_VIDEO;
+    }
+
+    public function setFileType($fileType)
+    {
+        if ( ! in_array($fileType, array(self::FILETYPE_X264, self::FILETYPE_WEBM, self::FILETYPE_OGG)))
+        {
+            throw new Exception\InvalidArgumentException(sprintf('Invalid format %s', $fileType));
+        }
+
+        $this->fileType = $fileType;
+    }
+
+    public function setVideoCodec($audioCodec)
+    {
+        $this->audioCodec = $audioCodec;
+    }
+
+    public function getVideoCodec()
+    {
+        return $this->audioCodec;
+    }
+
+    public function setDimensions($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+}
