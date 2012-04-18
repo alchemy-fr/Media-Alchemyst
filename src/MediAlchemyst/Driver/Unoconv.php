dbc8a09648fbda8fd7d63535f2c1f27979e260ac
Romain Neutron <imprec@gmail.com>
2012-04-18 20:09:31 +0200

    Add Unoconv driver

---
+++ src/MediAlchemyst/Driver/Unoconv.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MediAlchemyst\Driver;
+
+use Monolog\Logger;
+use Unoconv\Unoconv as UnoconvBinary;
+
+class Unoconv extends Provider
+{
+
+    protected $driver;
+
+    public function __construct(Logger $logger, $use_binary = null)
+    {
+        $this->logger = $logger;
+
+        if ($use_binary)
+        {
+            $this->driver = new UnoconvBinary($use_binary, $logger);
+        }
+        else
+        {
+            try
+            {
+                $this->driver = UnoconvBinary::load($logger);
+            }
+            catch (Exception\BinaryNotFoundException $e)
+            {
+                throw new Exception\RuntimeException('No driver available');
+            }
+        }
+    }
+
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+
+}
