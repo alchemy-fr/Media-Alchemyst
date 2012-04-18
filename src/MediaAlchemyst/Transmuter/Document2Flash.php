d57fc0fc3bcda9ed0eaa4047e55741963e14e00b
Romain Neutron <imprec@gmail.com>
2012-04-18 21:33:08 +0200

    Add document2Flash Transmuter

---
+++ src/MediaAlchemyst/Transmuter/Document2Flash.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MediaAlchemyst\Transmuter;
+
+use MediaAlchemyst\Specification;
+use MediaAlchemyst\Exception;
+use MediaVorus\Media\Media;
+
+class Document2Flash extends Provider
+{
+
+    public function execute(Specification\Provider $spec, Media $source, $dest)
+    {
+        if ( ! $spec instanceof Specification\Flash)
+        {
+            throw new Exception\SpecNotSupportedException('SwfTools only accept Flash specs');
+        }
+
+        $tmpDest = tempnam(sys_get_temp_dir(), 'pdf2swf');
+
+        /**
+         * unoconv =>pdf
+         * imagine => image
+         */
+        $this->container->getUnoconv()
+          ->open($source->getFile()->getPathfile())
+          ->saveAs(\Unoconv\Unoconv::TYPE_PDF, $tmpDest)
+          ->close();
+
+        $this->container->getPdf2Swf()
+          ->toSwf(new \SplFileInfo($tmpDest), $dest );
+
+        unlink($tmpDest);
+    }
+
+}
---
+++ tests/src/MediaAlchemyst/Transmuter/Document2FlashTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MediaAlchemyst\Transmuter;
+
+class Document2FlashTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Document2Flash
+     */
+    protected $object;
+    protected $specs;
+    protected $source;
+    protected $dest;
+
+    protected function setUp()
+    {
+        $this->object = new Document2Flash(new \MediaAlchemyst\DriversContainer(new \Symfony\Component\DependencyInjection\ParameterBag\ParameterBag(array())));
+
+        $this->specs = new \MediaAlchemyst\Specification\Flash();
+
+        $this->source = \MediaVorus\MediaVorus::guess(new \SplFileInfo(__DIR__ . '/../../../files/Hello.odt'));
+        $this->dest = __DIR__ . '/../../../files/output.swf';
+    }
+
+    protected function tearDown()
+    {
+        if (file_exists($this->dest) && is_writable($this->dest))
+        {
+            unlink($this->dest);
+        }
+    }
+
+    /**
+     * @covers MediaAlchemyst\Transmuter\Document2Flash::execute
+     */
+    public function testExecute()
+    {
+        $this->object->execute($this->specs, $this->source, $this->dest);
+
+        $MediaDest = \MediaVorus\MediaVorus::guess(new \SplFileInfo($this->dest));
+
+        $this->assertEquals(\MediaVorus\Media\Media::TYPE_FLASH, $MediaDest->getType());
+    }
+
+    /**
+     * @covers MediaAlchemyst\Transmuter\Document2Flash::execute
+     * @expectedException MediaAlchemyst\Exception\SpecNotSupportedException
+     */
+    public function testExecuteWrongSpecs()
+    {
+        $this->specs = new \MediaAlchemyst\Specification\Video();
+        $this->object->execute($this->specs, $this->source, $this->dest);
+    }
+
+}
