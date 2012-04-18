440e8c6a496dac1936558c7e95c8c1428e459308
Romain Neutron <imprec@gmail.com>
2012-04-18 17:43:11 +0200

    Add video2image transmuter

---
+++ src/MediAlchemyst/Transmuter/Video2Image.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MediAlchemyst\Transmuter;
+
+use MediAlchemyst\Specification;
+use MediAlchemyst\Exception;
+use MediaVorus\Media\Media;
+
+class Video2Image extends Provider
+{
+
+    public static $time = '60%';
+
+    public function execute(Specification\Provider $spec, Media $source, $dest)
+    {
+        if ( ! $spec instanceof Specification\Image)
+        {
+            throw new Exception\SpecNotSupportedException('FFMpeg Adapter only supports Video specs');
+        }
+
+        /* @var $spec \MediAlchemyst\Specification\Image */
+
+        $tmpDest = tempnam(sys_get_temp_dir(), 'ffmpeg');
+
+        $time = (int) ($source->getDuration() * $this->parseTimeAsRatio(static::$time));
+
+        $this->container->getFFMpeg()
+          ->open($source->getFile()->getPathname())
+          ->extractImage($time, $tmpDest)
+          ->close();
+
+        $image = $this->container->getImagine()->open($tmpDest);
+
+        if ($spec->getWidth() && $spec->getHeight())
+        {
+            $box   = new \Imagine\Image\Box($spec->getWidth(), $spec->getHeight());
+            $image = $image->resize($box);
+        }
+
+        $image->save($dest);
+
+        unlink($tmpDest);
+    }
+
+    protected function parseTimeAsRatio($time)
+    {
+        if (substr($time, -1) === '%')
+        {
+            return substr($time, 0, strlen($time) - 1) / 100;
+        }
+
+        return Max(Min((float) $time, 1), 0);
+    }
+
+}
