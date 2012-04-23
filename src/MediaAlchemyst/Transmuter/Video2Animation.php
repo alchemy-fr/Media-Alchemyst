ebbf1e739f23b64da5ab8270af01d7e2fdb1fc8d
Romain Neutron <imprec@gmail.com>
2012-04-24 00:09:31 +0200

    Add  Video2Animation transmuter

---
+++ src/MediaAlchemyst/Transmuter/Video2Animation.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace MediaAlchemyst\Transmuter;
+
+use MediaAlchemyst\Specification;
+use MediaAlchemyst\Exception;
+use MediaVorus\Media\Media;
+use Imagine\Image;
+
+class Video2Animation extends Provider
+{
+
+    public static $autorotate             = false;
+    public static $lookForEmbeddedPreview = false;
+
+    public function execute(Specification\Provider $spec, Media $source, $dest)
+    {
+        if ( ! $spec instanceof Specification\Animation)
+        {
+            throw new Exception\SpecNotSupportedException('Imagine Adapter only supports Image specs');
+        }
+
+        if ($source->getType() !== Media::TYPE_VIDEO)
+        {
+            throw new Exception\SpecNotSupportedException('Imagine Adapter only supports Images');
+        }
+
+        if ( ! class_exists('\\Gmagick'))
+        {
+            throw new Exception\RuntimeException('Gmagick is required for animated gif');
+        }
+
+        $movie = $this->container->getFFMpeg()->open($source->getFile()->getPathname());
+
+        $duration = $source->getDuration();
+
+        $time  = $pas   = Max(1, $duration / 11);
+        $files = array();
+
+        while ($time < $duration)
+        {
+            $files[] = $tmpFile = tempnam(sys_get_temp_dir(), 'ffmpeg');
+            $movie->extractImage(round($time), $tmpFile);
+            $time += $pas;
+        }
+
+        $movie->close();
+
+        $GIF = new \Gmagick();
+
+        $GIF->readImage(array_shift($files));
+        $GIF->setImageFormat('GIF');
+        $GIF->setImageDelay(80);
+        $GIF->nextImage();
+
+        foreach ($files as $file)
+        {
+            $frame = new \Gmagick();
+
+            $frame->readImage($file);
+            $frame->setImageFormat('GIF');
+            $frame->setImageDelay(80);
+
+            $GIF->addImage($frame);
+            $GIF->nextImage();
+
+            $frame->clear();
+            $frame->destroy();
+        }
+
+        $GIF->writeimage($dest);
+        $GIF->clear();
+        $GIF->destroy();
+
+        return $this;
+    }
+
+}
