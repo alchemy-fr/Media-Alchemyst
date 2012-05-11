0dabace05044754a9c2837032838ce4d023c0e5b
Romain Neutron <imprec@gmail.com>
2012-05-11 20:05:41 +0200

    Fix temporary filename

--- src/MediaAlchemyst/Transmuter/Video2Animation.php
+++ src/MediaAlchemyst/Transmuter/Video2Animation.php
@@ -9,39 +9,33 @@ use Imagine\Image;
 
 class Video2Animation extends Provider
 {
-
-    public static $autorotate             = false;
+    public static $autorotate = false;
     public static $lookForEmbeddedPreview = false;
 
     public function execute(Specification\Provider $spec, Media $source, $dest)
     {
-        if ( ! $spec instanceof Specification\Animation)
-        {
+        if ( ! $spec instanceof Specification\Animation) {
             throw new Exception\SpecNotSupportedException('Imagine Adapter only supports Image specs');
         }
 
-        if ($source->getType() !== Media::TYPE_VIDEO)
-        {
+        if ($source->getType() !== Media::TYPE_VIDEO) {
             throw new Exception\SpecNotSupportedException('Imagine Adapter only supports Images');
         }
 
-        if ( ! class_exists('\\Gmagick'))
-        {
+        if ( ! class_exists('\\Gmagick')) {
             throw new Exception\RuntimeException('Gmagick is required for animated gif');
         }
 
-        try
-        {
+        try {
             $movie = $this->container->getFFMpeg()->open($source->getFile()->getPathname());
 
             $duration = $source->getDuration();
 
-            $time  = $pas   = Max(1, $duration / 11);
+            $time = $pas = Max(1, $duration / 11);
             $files = array();
 
-            while ($time < $duration)
-            {
-                $files[] = $tmpFile = tempnam(sys_get_temp_dir(), 'ffmpeg');
+            while ($time < $duration) {
+                $files[] = $tmpFile = tempnam(sys_get_temp_dir(), 'ffmpeg') . '.jpg';
                 $movie->extractImage(round($time), $tmpFile);
                 $time += $pas;
             }
@@ -55,8 +49,7 @@ class Video2Animation extends Provider
             $GIF->setImageDelay(80);
             $GIF->nextImage();
 
-            foreach ($files as $file)
-            {
+            foreach ($files as $file) {
                 $frame = new \Gmagick();
 
                 $frame->readImage($file);
@@ -73,17 +66,12 @@ class Video2Animation extends Provider
             $GIF->writeimage($dest);
             $GIF->clear();
             $GIF->destroy();
-        }
-        catch (\FFMpeg\Exception\Exception $e)
-        {
+        } catch (\FFMpeg\Exception\Exception $e) {
             throw new Exception\RuntimeException($e->getMessage(), $e->getCode(), $e);
-        }
-        catch (\GmagickException $e)
-        {
+        } catch (\GmagickException $e) {
             throw new Exception\RuntimeException($e->getMessage(), $e->getCode(), $e);
         }
 
         return $this;
     }
-
 }
