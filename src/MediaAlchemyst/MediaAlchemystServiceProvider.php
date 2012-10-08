33d0cd906c4185ba7e854ec22736bdbae9e1c44e
Romain Neutron <imprec@gmail.com>
2012-10-08 14:05:04 +0200

    Enhance MediaAlchemyst service provider

--- src/MediaAlchemyst/MediaAlchemystServiceProvider.php
+++ src/MediaAlchemyst/MediaAlchemystServiceProvider.php
@@ -13,11 +13,55 @@ class MediaAlchemystServiceProvider implements ServiceProviderInterface
     {
         $app['media-alchemyst'] = $app->share(function(Application $app) {
 
-            if ( ! isset($app['mediavorus'])) {
+            if (!isset($app['mediavorus'])) {
                 throw new RuntimeException('Media-Alchemyst requires MediaVorus Service Provider');
             }
 
-            return new Alchemyst($app);
+            $drivers = new DriversContainer();
+
+            $drivers['mediavorus'] = function() use ($app) {
+                return $app['mediavorus'];
+            };
+
+            // Try to use official Monolog Service Provider
+            if (isset($app['monolog'])) {
+                $drivers['logger'] = function() use ($app) {
+                    return $app['monolog'];
+                };
+            }
+
+            // As exiftool is a dependency of mediavorus, use it
+            $drivers['exiftool.exiftool'] = function() use ($app) {
+                return $app['exiftool.processor'];
+            };
+
+            if (isset($app['imagine'])) {
+                $drivers['imagine'] = function() use ($app) {
+                    return $app['imagine'];
+                };
+            }
+
+            if (isset($app['ffmpeg.ffprobe'])) {
+                $drivers['ffmpeg.ffprobe'] = function() use ($app) {
+                    return $app['ffmpeg.ffprobe'];
+                };
+            }
+
+            if (isset($app['ffmpeg.ffmpeg'])) {
+                $drivers['ffmpeg.ffmpeg'] = function() use ($app) {
+                    return $app['ffmpeg.ffmpeg'];
+                };
+            }
+
+            foreach ($app->keys() as $key) {
+                if (strpos($key, 'media-alchemyst.') === 0) {
+                    $drivers[substr($key, 16)] = function() use ($app, $key) {
+                        return $app[$key];
+                    };
+                }
+            }
+
+            return new Alchemyst($drivers);
         });
     }
 
