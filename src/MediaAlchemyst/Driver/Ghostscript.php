6b315c32fee8ed18a3e954c36369693313448197
Romain Neutron <imprec@gmail.com>
2012-10-24 10:08:47 +0200

    Add missing ghostscript driver

---
+++ src/MediaAlchemyst/Driver/Ghostscript.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MediaAlchemyst\Driver;
+
+use Monolog\Logger;
+use Ghostscript\PDFTranscoder;
+use Ghostscript\Exception\ExceptionInterface as GhostscriptException;
+use MediaAlchemyst\Exception;
+
+class Ghostscript extends Provider
+{
+    protected $driver;
+
+    public function __construct(Logger $logger, $useBinary = null)
+    {
+        $this->logger = $logger;
+
+        if ($useBinary) {
+            $this->driver = new PDFTranscoder($useBinary, $this->logger);
+        } else {
+            try {
+                $this->driver = PDFTranscoder::load($this->logger);
+            } catch (GhostscriptException $e) {
+                throw new Exception\RuntimeException('No driver available');
+            }
+        }
+    }
+
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+}
