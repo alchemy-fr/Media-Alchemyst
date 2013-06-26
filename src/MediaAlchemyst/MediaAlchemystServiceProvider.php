30549c3c87aaabec268693fe55c7d84b6088a73e
Romain Neutron <imprec@gmail.com>
2013-06-26 11:43:55 +0200

    Revert logger customization

--- src/MediaAlchemyst/MediaAlchemystServiceProvider.php
+++ src/MediaAlchemyst/MediaAlchemystServiceProvider.php
@@ -10,10 +10,15 @@ class MediaAlchemystServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $app['media-alchemyst.configuration'] = array();
+        $app['media-alchemyst.logger'] = null;
 
         $app['media-alchemyst.drivers'] = $app->share(function (Application $app){
             $drivers = DriversContainer::create();
             $drivers['configuration'] = $app['media-alchemyst.configuration'];
+
+            if (null !== $app['media-alchemyst.logger']) {
+                $drivers['logger'] = $app['media-alchemyst.logger'];
+            }
 
             return $drivers;
         });
